89ad15782c89fba1a6137266050c7e667840c896
SarabjitS <sab.walia@gmail..com>
2023-01-19 16:34:45 -0330

    Add Pattern 20

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -319,12 +319,6 @@ public class Main {
 		}
 	}
 
-//	        *				*
-//			*				*
-//			*		*		*
-//			*	*		*	*
-//			*				*
-
 	public static void Pattern20(int n) {
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
