1f76b8fbe1d8bb174c46c2d0e906668b2661c94d
SarabjitS <sab.walia@gmail..com>
2023-01-19 16:32:49 -0330

    Pattern 20

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -44,6 +44,8 @@ public class Main {
 				break;
 			case 19:Pattern19(n);
 				break;
+			case 20:Pattern20(n);
+				break;
 		}
 	}
 
@@ -315,10 +317,29 @@ public class Main {
 
 			System.out.println();
 		}
-
 	}
 
-}
+//	        *				*
+//			*				*
+//			*		*		*
+//			*	*		*	*
+//			*				*
+
+	public static void Pattern20(int n) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (j == 1 || j == n) {
+						System.out.print("*\t");
+					} else if (i > n / 2 && (i == j || i + j == n + 1)) {
+						System.out.print("*\t");
+					} else {
+						System.out.print("\t");
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
 
 
 
